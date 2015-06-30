dc0eee7baaff67018e9a5b61c26ea2ac5e9b2c17
Javi Fernández <jfernandezriverola@gmail.com>
2015-06-30 19:53:54 +0200

    more

--- lib/sorcery/adapters/active_record_adapter.rb
+++ lib/sorcery/adapters/active_record_adapter.rb
@@ -67,8 +67,8 @@ module Sorcery
               relation = relation.or(condition)
             end
 
-            if !@klass.sorcery_config.scoped_login_attribute_name.nil?
-              scoped_condition = @klass.arel_table[scoped_login_attribute].eq(credentials[3])
+            if credentials[3]
+              scoped_condition = @klass.arel_table["company_id"].eq(credentials[3])
               relation = relation.and(scoped_condition)
             end
 
